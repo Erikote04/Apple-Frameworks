270e60e8157756cd5c409b31b214276270a245c0
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-15 17:22:36 +0200

    Creating the Framework model

---
+++ Apple-Frameworks/Apple-Frameworks/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  Apple-Frameworks
+//
+//  Created by Erik Sebastian de Erice Jerez on 15/10/24.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
