257520c880794f9671aef2d903813757105c352f
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-16 00:19:56 +0200

    Created a tab view to switch between grid and list

---
+++ Apple-Frameworks/Apple-Frameworks/View/FrameworkTabView.swift
@@ -0,0 +1,31 @@
+//
+//  FrameworkTabView.swift
+//  Apple-Frameworks
+//
+//  Created by Erik Sebastian de Erice Jerez on 16/10/24.
+//
+
+import SwiftUI
+
+struct FrameworkTabView: View {
+    var body: some View {
+        TabView {
+            FrameworkGridView()
+                .tabItem {
+                    Image(systemName: "square.grid.3x3.fill")
+                    Text("Grid")
+                }
+            
+            FrameworkListView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("List")
+                }
+        }
+        .tint(Color(.label))
+    }
+}
+
+#Preview {
+    FrameworkTabView()
+}
